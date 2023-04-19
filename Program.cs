64a0d5748c0b9bf3c9a8c26544ac1398ae8f744c
AZ <speedsterk@hotmail.com>
2023-04-19 22:28:07 +0300

    IConfigRepo from assembly

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Simplz.Hue.Data;
 using Simplz.Hue.Services;
 
@@ -7,7 +8,19 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddScoped<HueService>();
-builder.Services.AddScoped<IConfigRepo, ConfigRepo>();
+
+builder.Services.AddScoped<IConfigRepo>((serviceProvider) =>
+{
+    Type interfaceType = typeof(IConfigRepo);
+    var configRepoType = Assembly.GetExecutingAssembly().GetTypes().Where(t => interfaceType.IsAssignableFrom(t) && t.IsClass).FirstOrDefault();
+    if (configRepoType is not null)
+    {
+        var instance = (IConfigRepo?)Activator.CreateInstance(configRepoType);
+        if (instance is not null)
+            return instance;
+    }
+    throw new NotImplementedException("IConfigRepo needs to have an implementation in the assembly");
+});
 
 await using var app = builder.Build();
 
