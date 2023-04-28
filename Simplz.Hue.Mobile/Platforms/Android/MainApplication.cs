f54a81a41017e4a5036bb2f152486beb0a077286
kapiosk <29824083+kapiosk@users.noreply.github.com>
2023-04-28 16:58:43 +0300

    android workaround for http

--- Simplz.Hue.Mobile/Platforms/Android/MainApplication.cs
+++ Simplz.Hue.Mobile/Platforms/Android/MainApplication.cs
@@ -3,7 +3,7 @@ using Android.Runtime;
 
 namespace Simplz.Hue.Mobile;
 
-[Application]
+[Application(UsesCleartextTraffic = true)]
 public class MainApplication : MauiApplication
 {
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
