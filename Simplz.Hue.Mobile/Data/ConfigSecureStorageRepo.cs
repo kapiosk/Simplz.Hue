1692f7c558fc8e67e66aa0f50cdb8596d95cfea7
AZ <speedsterk@hotmail.com>
2023-04-28 16:23:44 +0300

    .

---
+++ Simplz.Hue.Mobile/Data/ConfigSecureStorageRepo.cs
@@ -0,0 +1,28 @@
+﻿using Simplz.Hue.Data;
+
+namespace Simplz.Hue.Mobile.Data
+{
+    internal class ConfigSecureStorageRepo : IConfigRepo
+    {
+        public async Task<string> GetBridgeIPAsync()
+        {
+            var ip = await SecureStorage.Default.GetAsync("ip");
+            return ip;
+        }
+
+        public async Task<string> GetKeyAsync()
+        {
+            return await SecureStorage.Default.GetAsync("key");
+        }
+
+        public async Task SetBridgeIPAsync(string ip)
+        {
+            await SecureStorage.Default.SetAsync("ip", ip);
+        }
+
+        public async Task SetKeyAsync(string key)
+        {
+            await SecureStorage.Default.SetAsync("key", key);
+        }
+    }
+}
